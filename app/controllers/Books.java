16672831cd63e6a8057ffbb7d5bb776bb83bd7e3
Thomas Toye <toye.thomas@gmail.com>
2014-08-03 19:05:32 +0200

    Add method stubs

---
+++ app/controllers/Books.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import scala.NotImplementedError;
+
+public class Books extends Controller {
+	
+	/**
+	 * List all books
+	 * @return
+	 */
+	public static Result list() {
+		return TODO;
+	}
+	
+	/**
+	 * List details for a given book
+	 * @param id The book
+	 * @return
+	 */
+	public static Result details(long id) {
+		return TODO;
+	}
+	
+	/**
+	 * Show form to add a new Book
+	 * @return
+	 */
+	public static Result newBook() {
+		return TODO;
+	}
+	
+	/**
+	 * Show filled form to edit an existing book
+	 * @param id The id of the Book
+	 * @return
+	 */
+	public static Result edit(long id) {
+		return TODO;
+	}
+	
+	/**
+	 * Save a book by binding a request
+	 * @return
+	 */
+	public static Result save() {
+		return TODO;
+	}
+}
