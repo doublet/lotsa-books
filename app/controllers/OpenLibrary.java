a85fff5fae59954a975f13eb49c3c7ea61ec7ce1
Thomas Toye <toye.thomas@gmail.com>
2014-08-05 22:08:41 +0200

    Add method to retrieve a list of book informations for a query

--- app/controllers/OpenLibrary.java
+++ app/controllers/OpenLibrary.java
@@ -1,11 +1,17 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 import play.mvc.Controller;
 import play.libs.WS;
 import play.mvc.Result;
-
 import static play.libs.F.Function;
 import static play.libs.F.Promise;
+import models.*;
 
 public class OpenLibrary extends Controller {
 	
@@ -19,4 +25,32 @@ public class OpenLibrary extends Controller {
 	    );
 	    return resultPromise;
 	}
+
+	public static Promise<List<BookInfo>> getBookInfo(String query) {
+	    final Promise<List<BookInfo>> resultPromise = WS.url("http://openlibrary.org/search.json").setQueryParameter("q", query).get().map(
+	            new Function<WS.Response, List<BookInfo>>() {
+	                public List<BookInfo> apply(WS.Response response) {
+	                	JsonNode results = response.asJson().findPath("docs");
+	                    ArrayList<BookInfo> res = new ArrayList<>();
+	                    Iterator<JsonNode> it = results.elements();
+	                    
+	                    while (it.hasNext()) {
+							JsonNode json = it.next();
+							if(json == null) continue;
+							// build new BookInfo
+							BookInfo toAdd = new BookInfo();
+							
+							toAdd.title = json.findValue("title").asText();
+							toAdd.author_name = json.findValue("author_name").asText();
+							res.add(toAdd);
+							
+						}
+	                    
+	                	return res;
+	                }
+	            }
+	    );
+	    return resultPromise;
+	}
+	
 }
