645dfa0b80aff18cf62fd019a233a556d59615ea
Thomas Toye <toye.thomas@gmail.com>
2014-08-12 19:48:50 +0200

    Add validate method for Form validation and rename old one

--- app/models/Isbn.java
+++ app/models/Isbn.java
@@ -1,10 +1,14 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import org.apache.commons.validator.routines.ISBNValidator;
 
+import play.data.validation.ValidationError;
 import play.db.ebean.Model;
 
 @Entity
@@ -14,8 +18,14 @@ public class Isbn extends Model {
 	
 	public String isbn;
 	
-    public static boolean validate(String input) {
+    public static boolean isValid(String input) {
         ISBNValidator validator = new ISBNValidator();
         return validator.isValid(input);
+    }
+    
+    public List<ValidationError> validate() {
+    	List<ValidationError> errors = new ArrayList<ValidationError>();
+    	if(!Isbn.isValid(this.isbn)) errors.add(new ValidationError("isbn", "Malformed ISBN"));
+    	return errors.isEmpty() ? null : errors;
     }
 }
