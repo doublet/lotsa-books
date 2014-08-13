b203548ef9a2071ca7929e777804cb914f6b9050
Thomas Toye <toye.thomas@gmail.com>
2014-08-13 19:29:31 +0200

    Correct typo

--- app/controllers/OpenLibrary.java
+++ app/controllers/OpenLibrary.java
@@ -16,7 +16,7 @@ import models.*;
 
 public class OpenLibrary extends Controller {
 	
-	public static Promise<Result> numatchingItems(String query) {
+	public static Promise<Result> numMatchingItems(String query) {
 	    final Promise<Result> resultPromise = WS.url("http://openlibrary.org/search.json").setQueryParameter("q", query).get().map(
 	            new Function<WS.Response, Result>() {
 	                public Result apply(WS.Response response) {
